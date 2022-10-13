d444182ff70f015289d789004132aabbc05b1eaf
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-13 13:54:00 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Description: main - is void
- * return: Always 0 (Success)
+ * Description: main -Prints out a string to stdout
+ * return: 0 if success
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
