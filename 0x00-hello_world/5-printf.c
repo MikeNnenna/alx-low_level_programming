6deaba69316bf347311f5d5aaa9dddc32c678cd9
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-13 14:41:52 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Starting point of the program
+ * Description: enty point of the program
+ * Return: always 0 if success
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	retun (0);
+}
