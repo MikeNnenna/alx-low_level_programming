1d7ceedd5dc90f3186441a80655e37beaaf069fa
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-13 15:40:05 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - ntry point
+ * Description: code starting point
+ * Return: 1 if success
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
