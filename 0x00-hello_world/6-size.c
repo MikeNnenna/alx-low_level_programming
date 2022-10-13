60d493f18209ef7d8a1c6aa01eba79f4dff076c7
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-13 15:04:42 -0700

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ * Description: starting point of code
+ * Return: 0 if success
+ */
+int main(void)
+{
+	printf("size of char: %d byte(s)\n", size of (char));
+	printf("size of int: %d byte(s) \n", size of (int));
+	printf("size of long int: %d byte(s\n", size of (long int));
+	printf("size of long long int: %d byte(s)", size of (long long int));
+	peintf("size of float: %d byte(s)", size of (float));
+	return (0);
+
