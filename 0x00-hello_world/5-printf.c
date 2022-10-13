9ffa03aecf819f0209f124e22c975eab1f69fdf3
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-13 14:45:26 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * Description: enty point of the program
  * Return: always 0 if success
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
