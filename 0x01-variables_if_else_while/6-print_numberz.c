d1e30ed9a42629a0734aaa36a31e6b3dd736fc48
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-14 12:03:58 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
