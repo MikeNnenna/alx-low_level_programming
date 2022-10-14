875b890e82ce75341b6900ca756b73ac6e092363
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-14 09:20:00 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	char abc;
+
+	for(abc='a'; abc<='z'; abc++)
+	{
+		putchar(abc);
+			
+	}
+	putchar(\n);
+	return (0);
+
+}
