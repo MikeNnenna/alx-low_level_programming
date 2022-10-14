00c0af5a3ff6705ed5dbb06e21cf442cf59ea201
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-14 10:19:12 -0700

    go

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	char(abc);
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+		if (abc = 'q' || abc = 'e')
+	{
+		continue;
+	}
+		putchar(abc);
+	}
+	putchar('\n');
+	return (0);
+}
