07e8a73bf778fa1761e54811c2d138e4e4da71e4
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-16 11:23:51 -0700

    print bas 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int num;
+	int num1;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num);
+	}
+	for (num1 = a; num <= f; num1++)
+	{
+		putchar(num1);
+	}
+	putchar('\n');
+	retun (0);
+}
