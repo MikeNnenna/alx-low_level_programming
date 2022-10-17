d0522fb336ba18527008f864ecccb40641486e24
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-17 03:07:35 -0700

    combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,18 +8,19 @@
 int main(void)
 {
 	int x;
+	
 	for (x = '0'; x <= '9'; x++)
 	{
 		putchar(x);
-	}
-	if (x == '9');
-	{
-		continue;
-	}
-	else
-	{
-		putchar(',');
-		putchar(' ');
+		if (x == '9')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
