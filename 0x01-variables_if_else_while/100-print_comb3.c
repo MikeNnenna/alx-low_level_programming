43528db3ea2c2ad45f70f9585f3b0977b6e03944
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-17 12:06:35 -0700

    go

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (x = '0'; x <= '9'; x++)
+		{
+			putchar(x);
+			putchar(y);
+		}
+		if (x = '9' && y = '9')
+		{
+			continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return(0)
+}
