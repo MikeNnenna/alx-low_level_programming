d6c525d428eda2afd709c1296c29912fecc216a5
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-18 13:03:21 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
  * _putchar - writes the character _putchar to stdout
  * Return: 0 if success
  */
-int _putchar(char _putchar);
+int _putchar(char _putchar)
 {
 
 	return (writes (1, &_putchar, 1))
