9e30ba045330805ae91baf724d7e1e8c79800cee
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-19 00:22:33 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int main;
+	for (a = 'a'; a <= 'z', a++)
+	{
+		_putchar('a');
+	}
+	_putchar('\n');
+	retun (0)
+}
