a1ae840cbe500cbb058d089eb509c0b39ada5321
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-19 01:04:52 -0700

    alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,11 +12,12 @@ void print_alphabet_x10(void)
 	char i;
 	x = 0;
 
-	while (x <= 10)
+	while (x < 10)
 	{
 		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
 		_putchar('\n');
 	}
 	x++;
+	return (0);
 }
