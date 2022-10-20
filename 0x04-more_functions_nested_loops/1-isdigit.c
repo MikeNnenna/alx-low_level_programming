c1f61c8bfe139ef33699e50178811bbbffb2017a
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-20 13:26:39 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digits
+ * @c: prints in
+ * Return: 0 || 1 if successful
+ */
+
+int _isdigit(int c);
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
