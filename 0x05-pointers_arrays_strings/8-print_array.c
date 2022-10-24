347201add108b5f723b6f3642ecee5693df6272e
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-24 10:50:49 -0700

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * void print_array - prints some numbers in the array
+ * @a: an array
+ * @n: last integer
+ */
+void print_array(int *a, int n)
+{
+	int count;
+	
+	count = 0;
+	
+	while (count < n)
+	{
+		if (count != (n - 1))
+			printf("%i, ", a[count]);
+		else
+			printf("%i", a[count]);
+		count++;
+	}
+	printf("\n");
+}
