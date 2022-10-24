8418fea5bea5279685d2488bbf6dcb0dd3529383
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-24 08:44:49 -0700

    go

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,15 +7,14 @@
 
 void _puts(char *str)
 {
-	int x;
+	int i;
 	
-	x = 0;
+	i = 0;
 	
-	while (str[x] != '\0')
+	while (str[i] != '\0')
 	{
-		_putchar(str[x]);
-		x++;
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
-	return (0);
 }
