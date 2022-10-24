e01b796f45b2990e37028582109ed1160af32c7f
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-24 11:02:03 -0700

    go

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strcpy - copies string
+ * @dest: char pointer
+ * @src: second char pointer
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, count;
+	len = 0;
+	
+	while (src[len] != '\0')
+		len++;
+	for (count = 0; count <= len; count++)
+		dest[count] = src[count];
+	return (dest);
+}
