605af03f0525056bfdb47f7d19f77823d0dba75d
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-24 08:20:10 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string, followed by a new line
+ * @str: the string
+ * Return: 0 always
+ */
+
+void _puts(char *str)
+{
+	int x;
+
+	x = 0;
+
+	while (str[x] != '\0')
+	{
+		i++;
+	}
+	_putchar('\n');
+	return (0)
+}
