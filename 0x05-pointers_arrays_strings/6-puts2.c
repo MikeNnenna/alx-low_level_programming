e7e09de6a6e4190f532465099b298c443d7d9c48
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-24 10:06:13 -0700

    go

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character
+ * @str: the string
+ */
+void puts2(char *str)
+{
+	int x;
+
+	x = 0;
+
+	while (s[x] != '\0')
+	{
+		if (x % 2 == 0)
+		{
+			_putchar(s[x]);
+			x++;
+		}
+		putchar('\n');
+	}
+}
