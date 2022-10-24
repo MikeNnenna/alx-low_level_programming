eccdc885a1807073ee8a6c3f596de9592b61b1af
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-24 07:46:05 -0700

    pleeasee

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets the pointer to 98
+ * @n: the value to be reset
+ * Return: 0 always
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef Main_h
+#define Main_h
+
+void reset_to_98(int *n);
+
+#endif
