82183f8def60eac0eec64bdfed0d81e11d1b8632
Mike-Ajanwachuku <rosemajanwachuku@gmail.com>
2022-10-24 10:34:56 -0700

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,11 +10,11 @@ void puts_half(char *str)
 		int i, half;
 
 		i = 0;
-		
+
 		while (str[i] != '\0')
 			i++;
 		half = (i) / 2;
-		
+
 		if (i % 2 != 0)
 			half = ((i - 1) / 2) + 1;
 		for (; half < i; half++)
